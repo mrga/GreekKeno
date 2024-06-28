e7f866241c47e8752de52d58953baa2e006ad063
mrga <mrganic@gmail.com>
2024-06-28 18:28:08 +0200

    Initial Commit

---
+++ GK/GKApp.swift
@@ -0,0 +1,17 @@
+//
+//  GKApp.swift
+//  GK
+//
+//  Created by Branislav Mrganic on 28.6.24..
+//
+
+import SwiftUI
+
+@main
+struct GKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
